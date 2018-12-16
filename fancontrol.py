fd01fc8d096b6611294188c45aa934d9e93ed449
agithug <45919964+agithug@users.noreply.github.com>
2018-12-16 17:17:22 +0000

    Create fancontrol.py
    Stub , will add extra stuff to make it work

---
+++ fancontrol.py
@@ -0,0 +1,11 @@
+while True:
+    if temp > 45:
+        try:
+            fan.on()
+        except:
+            pass
+    else:
+        try:
+            fan.on()
+        except:
+            pass
