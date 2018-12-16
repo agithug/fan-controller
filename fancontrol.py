bfff76595576ef2128aa9a8d77c3dbdb659bcf36
agithug <45919964+agithug@users.noreply.github.com>
2018-12-16 17:21:48 +0000

    Update fancontrol.py

--- fancontrol.py
+++ fancontrol.py
@@ -1,3 +1,11 @@
+import os 
+
+# Return CPU temperature as a character string                                      
+def getCPUtemperature():
+    res = os.popen('vcgencmd measure_temp').readline()
+    return(res.replace("temp=","").replace("'C\n",""))
+
+
 while True:
     if temp > 45:
         try:
